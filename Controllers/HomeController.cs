5f5a8683de0184db8162a0349cc1f6085ae116ec
ahmed2786 <65840251+ahmed2786@users.noreply.github.com>
2024-05-29 12:17:48 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using IronPdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PdfGeneratorDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "/api/[controller]/[action]")]
+        public IActionResult DownloadChartPdf()
+        {
+            SavePdf();
+
+            string path = Path.Combine(Environment.CurrentDirectory, @"Pdf\", "js-chart.pdf");
+
+            if (System.IO.File.Exists(path))
+            {
+                return File(System.IO.File.OpenRead(path), "application/octet-stream", Path.GetFileName(path));
+            }
+            return NotFound();
+        }
+        [NonAction]
+        public void SavePdf()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"Html\", "htmlpage.html");
+            string htmlWithJs = System.IO.File.ReadAllText(path);
+
+            string json = @"{
+                    columns: [
+                        ['data1', 30, 200, 100, 400, 150, 250],
+                        ['data2', 50, 20, 10, 40, 15, 25]
+                    ]
+                }";
+
+            htmlWithJs = htmlWithJs.Replace("@JsonData", json);
+
+            // Instantiate Renderer
+            var htmlToPdf = new NReco.PdfGenerator.HtmlToPdfConverter();
+            var pdf = htmlToPdf.GeneratePdf(htmlWithJs);
+
+            var pdfFilePath = Path.Combine(Environment.CurrentDirectory, @"Pdf\", "js-chart.pdf");
+            // Export to a file or Stream
+            System.IO.File.WriteAllBytes(pdfFilePath, pdf);
+        }
+    }
+}
